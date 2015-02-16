48fbafa13cbbf23c11838ea8b606f59a71e96fc2
regimental12 <richard.hankins@gmail.com>
2015-02-16 19:32:08 +0000

    imageloader flesh out

--- src/ImageLoader.cpp
+++ src/ImageLoader.cpp
@@ -1,1 +1,27 @@
 #include "ImageLoader.h"
+
+ImageLoader::ImageLoader()
+{
+    surface = nullptr;
+}
+
+ImageLoader::~ImageLoader()
+{
+
+}
+
+int ImageLoader::getheight()
+{
+    return height;
+}
+
+int ImageLoader::getwidth()
+{
+    return width;
+}
+
+GLuint ImageLoader::LoadTexture(std::string filename)
+{
+    return texture;
+}
+
