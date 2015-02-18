f3ad3e3015d7bdc2cb8b9ecd645c378c74d4b5c1
GeorgeVicarey <g.vicarey@hotmail.co.uk>
2015-02-18 10:20:58 +0000

    Added World class skeleton.

---
+++ src/World.cpp
@@ -0,0 +1,21 @@
+#include "World.h"
+
+World::World()
+{
+
+}
+
+World::~World()
+{
+
+}
+
+void World::Render()
+{
+
+}
+
+void World::Update()
+{
+
+}
---
+++ src/World.h
@@ -0,0 +1,16 @@
+#ifndef SRC_WORLD_H_
+#define SRC_WORLD_H_
+
+class World
+{
+public:
+	World::World();
+	World::~World();
+	void World::Update();
+	void World::Render();
+private:
+};
+
+
+
+#endif /* SRC_WORLD_H_ */
