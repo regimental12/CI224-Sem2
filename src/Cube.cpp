867120ce2bc8b5c055e6fd7f5ff3475f3a10436b
regimental12 <richard.hankins@gmail.com>
2015-03-16 11:59:51 +0000

    moved render function to cube class

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -72,5 +72,40 @@ void Cube::loadCube()
     glEnableVertexAttribArray(2);
     std::cout  << "4" << std::endl;
     glBindVertexArray(0);
-    setTexture("../images/wall.jpg");
+    setTexture("images/wall.jpg");
 }
+
+void Cube::Render(Shader shader , Camera* camera)
+{
+    shader.useShader();
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    GLint modelLoc = glGetUniformLocation(shader.getProgram(), "model");
+    GLint viewLoc = glGetUniformLocation(shader.getProgram(), "view");
+    GLint projLoc = glGetUniformLocation(shader.getProgram(), "projection");
+    
+    /*glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));*/
+    
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(camera->view));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(camera->projection));
+    
+
+    glBindVertexArray(VAO);
+	 
+		  glBindTexture(GL_TEXTURE_2D, texture);
+		  glm::mat4 model;
+		  //model = glm::translate(model, position);
+		  GLfloat angle = 20.0f;
+
+		  model = glm::rotate(model, angle, glm::vec3(1.0f, 1.0f, 1.0f));
+
+		  //model = glm::rotate(model, angle, glm::vec3(1.0f, 1.0f, 0.5f));
+
+		  glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		  glDrawArrays(GL_TRIANGLES, 0, 36);
+		  glBindTexture(GL_TEXTURE_2D , 0);
+	
+    glBindVertexArray(0);
+}
+
