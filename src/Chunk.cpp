4c64270edc58f693763437d2ef5cee6c52a39b1e
GeorgeVicarey <g.vicarey@htomail.co.uk>
2015-03-22 13:34:41 +0000

    Chunk is optimised so that only cubes that have a neighbouring empty cube get rendered.

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -32,19 +32,31 @@ void Chunk::Init(){
 	}
 }
 
-void Chunk::Render(Shader shader, Camera* camera){	for (int x = 0; x < size.x; x++) {
-	for (int y = 0; y < size.y; y++) {
-		for (int z = 0; z < size.z; z++) {
-			if(x == 0 || x == size.x-1 ||
-					y == 0 || y == size.y-1||
-						z == 0 || z == size.z-1){
-				Cubes[x][y][z]->Render(shader, camera);
-			} else {
-//				Cubes[x][y][z]->Render(shader, camera);
+void Chunk::Render(Shader shader, Camera* camera) {
+	for (int x = 0; x < size.x; x++) {
+		for (int y = 0; y < size.y; y++) {
+			for (int z = 0; z < size.z; z++) {
+				// only attempt to render cubes that aren't NULL
+				if (Cubes[x][y][z]) {
+					// only render cubes that are on the very edges of the chunk.
+					if (x == 0 || x == size.x - 1 || y == 0 || y == size.y - 1
+							|| z == 0 || z == size.z - 1) {
+						//Cubes[x][y][z]->Render(shader, camera);
+					} else {
+						//check all inside cubes
+						//only render cubes if they'res an empty cube on any face
+						if (!Cubes[x + 1][y][z] || !Cubes[x - 1][y][z]
+								|| !Cubes[x][y + 1][z] || !Cubes[x][y - 1][z]
+								|| !Cubes[x][y][z + 1] || !Cubes[x][y][z - 1]) {
+							Cubes[x][y][z]->Render(shader, camera);
+						} else {
+
+						}
+					}
+				}
 			}
 		}
 	}
-}
 
 }
 
