74a73be2a7bd10dbf5f6b3d87c16f8ff08e63867
GeorgeVicarey <g.vicarey@htomail.co.uk>
2015-04-20 11:56:07 +0100

    changed raycasting to 1.0

--- src/Collision.h
+++ src/Collision.h
@@ -52,7 +52,7 @@ static bool RayCollision(glm::vec3 near , glm::vec3 far , Cube* cube)
       closestPoint = near+ lineDir * t;
     }
     
-    if(glm::distance(cube->getPosition() , closestPoint) < 0.2)
+    if(glm::distance(cube->getPosition() , closestPoint) < 1.0f)
     {
       std::cout << "rayhit" << std::endl;
       return true;
